50d7874aae43592d7deb02a2b1a20f3ed7d683a4
Trent Broome <62418290+trentbroome@users.noreply.github.com>
2021-03-19 23:00:58 -0400

    Update MainController.java

--- gs-serving-web-content-master (1)/gs-serving-web-content-master/complete/src/main/java/com/example/servingwebcontent/MainController.java
+++ gs-serving-web-content-master (1)/gs-serving-web-content-master/complete/src/main/java/com/example/servingwebcontent/MainController.java
@@ -1,11 +1,38 @@
 package com.example.servingwebcontent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
+
 @Controller
 public class MainController {
+
+   @Autowired
+   PatientRepository patientRepository;
+
+
+   @GetMapping("/createPatient")
+   public String createProjectForm(Model model) {
+      model.addAttribute("patient_list", patientRepository.findAll());
+      model.addAttribute("patient", new Patient());
+      return "createPatientOrder";
+   }
+
+   @PostMapping("/savePatientInfo")
+   public String addNewPatient(@ModelAttribute("patient") Patient patient, Model model)
+   {
+      patientRepository.save(patient);
+      return "redirect:/createPatient";
+   }
+
 
     @GetMapping("/login")
 	public String viewLoginPage(Model model)
@@ -20,7 +47,28 @@ public class MainController {
 
    @GetMapping("/home")
    public String adminPanel(Model model)
-   {
-	   return "adminPanel";
+   {       
+	   return "refDrPanel";
    }
+   /*@GetMapping("/radiologistsPanel")
+   public String radiologistsPanel(Model model)
+   {
+	   return "radiologistsPanel";
+   }
+   @GetMapping("/receptionistsPanel")
+   public String receptionistsPanel(Model model)
+   {
+	   return "receptionistsPanel";
+   }
+   @GetMapping("/refDrPanel")
+   public String refDrPanel(Model model)
+   {
+	   return "refDrPanel";
+   }
+   @GetMapping("/techPanel")
+   public String techPanel(Model model)
+   {
+	   return "techPanel";
+   }*/
+   
 }
