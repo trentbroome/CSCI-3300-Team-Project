f7121944e2971cf4b9bba5ab67aef03f957aa18d
Trent Broome <62418290+trentbroome@users.noreply.github.com>
2021-03-29 20:24:11 -0400

    Delete gs-serving-web-content-master (1)/gs-serving-web-content-master directory

--- gs-serving-web-content-master (1)/gs-serving-web-content-master/complete/src/main/java/com/example/servingwebcontent/OrderInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.servingwebcontent;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface OrderInfoRepository extends CrudRepository<OrderInfo, Integer> {
-    
-}
