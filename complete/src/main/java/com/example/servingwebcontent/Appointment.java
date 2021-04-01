d740566873f0598a8ec8023354a39842692b4fec
Bowens100 <77711399+Bowens100@users.noreply.github.com>
2021-03-31 23:41:30 -0400

    Update Appointment.java

--- complete/src/main/java/com/example/servingwebcontent/Appointment.java
+++ complete/src/main/java/com/example/servingwebcontent/Appointment.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
+
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "Appointment") //This specifies the primary table for the annotated entity in MySQL. MAKE SURE YOUR NAMES MATCH IN YOUR SQL 
@@ -13,13 +16,35 @@ public class Appointment {
     @Id //Specifies the primary key of an entity.
     @Column(name = "id") //Specifies the mapped column for a persistent property or field.
     @GeneratedValue(strategy=GenerationType.AUTO) //The GeneratedValue annotation may be applied to a primary key property or field of an entity or mapped superclass in conjunction with the Id annotation
-private String appttime;
+    private Long id;
 
-    public String getApptTime(){
+    @Column(name = "appttime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    public LocalDateTime appttime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public LocalDateTime getApptTime(){
         return appttime;
     }
 
-    public void setApptTime(String appttime){
+    public void setApptTime(LocalDateTime appttime){
+        this.appttime = appttime;
+    }
+
+    public Appointment(){
+
+    }
+
+    public Appointment(Long id, LocalDateTime appttime){
+        this.id = id;
         this.appttime = appttime;
     }
 }
