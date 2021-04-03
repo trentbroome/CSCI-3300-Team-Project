2469d5cc92290ac06afa982c1eea725b68b54fc9
Trent Broome <62418290+trentbroome@users.noreply.github.com>
2021-04-03 15:54:59 -0400

    Create AppointmentRepository.java

---
+++ complete/src/main/java/com/example/servingwebcontent/AppointmentRepository.java
@@ -0,0 +1,8 @@
+package com.example.servingwebcontent;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
+
+}
